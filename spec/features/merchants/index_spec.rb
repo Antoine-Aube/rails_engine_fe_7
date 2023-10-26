81fc813a94d9600a9661c476f2ead4f087dc3d41
Antoine Aube <aaube3@gmail.com>
2023-10-25 20:07:17 -0600

    test: very simple feature test for merchants index

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -1,13 +1,11 @@
-require 'rails-helper'
+require 'rails_helper'
 
 RSpec.describe "Merchants display pages" do 
   describe "when I visit /merchants" do 
-    it "Show the name of each merchants as a link to their show page" do 
-
-
+    it "Show the name of each merchants as a link to their show page", :vcr do 
       visit "/merchants"
 
-      expect(page).to have_link()
+      expect(page).to have_link("Klein, Rempel and Jones")
     end
   end
 end
